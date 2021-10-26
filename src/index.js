b48c407a1f9d75a1484609a54fca47d355eb32a6
yukaoi <aoyk.ooiooi@gmail.com>
2021-10-26 14:00:20 +0900

    increment simpleHTML

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// lodashの機能を使用する
+// 配列連結させて表示する
+const component = () => {
+ const element = document.createElement('div');
+ const array = ['hello', 'webpack'];
+ element.innerHTML = _.join(array, ' ')
+ return element;
+}
+
+document.body.appendChild(component());
